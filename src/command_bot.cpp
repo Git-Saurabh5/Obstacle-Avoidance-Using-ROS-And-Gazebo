23c39991fc586359fea388ade96a051416775063
Saurabh Suresh Powar <66636289+Git-Saurabh5@users.noreply.github.com>
2020-09-06 18:22:47 +0530

    Update command_bot.cpp

--- src/command_bot.cpp
+++ src/command_bot.cpp
@@ -2,62 +2,94 @@
 #include "std_msgs/String.h"
 #include "sensor_msgs/LaserScan.h"
 #include "geometry_msgs/Twist.h"
+#include <iostream>
+using namespace std; 
 
-float angY, angZ, linX;
+float sensors;
+float sensor_angle;
 
-void laserScanCallback(const sensor_msgs::LaserScan laser)
+
+void callback_laser(const sensor_msgs::LaserScan laser)
 {
-  //ROS_INFO("size[%d]: ", laser.ranges.size());
-  for (unsigned int index = 0; index < laser.ranges.size(); index++) {
-    //ROS_INFO("ranges[%f]: ", laser.ranges[i]);
-    if (laser.ranges[index] > 1 && laser.ranges[index] < 3) {
-      if (index > 6) {
-        //obstacle detected on left turn right/clockwise
-        ROS_INFO("TURN RIGHT");
-        linX = 0.05f;
-        angY = 1.0f;
-        angZ = 1.0f;
-      }
-      else {
-        //obstacle detected on right turn left/anticlockwise
-        ROS_INFO("TURN LEFT");
-        linX = 0.05f;
-        angY = 1.0f;
-        angZ = -1.0f;
-      }
-    }
-    else {
-      ROS_INFO("GO STRAIGHT");
-      linX = 0.15f;
-      angY = 0.0f;
-      angZ = 0.0f;
-    }
-  }
+ 
+	sensors = laser.ranges[0];
+	
+	for(int j=0;j<=10;j++){
+             if(laser.ranges[j]<sensors)
+			{
+				sensors = laser.ranges[j];
+				sensor_angle = j/2;
+			}
+           }
+
 }
 
+void motion(sensors, sensor_angle)
+{
+	
+        
+	if(sensors <= 1.0)  // min_range<=0.5 gave box pushing like behaviour, min_range<=1.2 gave obstacle avoidance
+	{
+		if(sensor_angle <= 1)
+		{
+			 cmdvel.angular.z=1.0;
+			 cmdvel.linear.x=0;
+			 cout<<"OBSTACLE AT RIGHT\n";
+		}
+        else if(sensor_angle > 1 && sensor_angle <= 2.5)
+		{
+			 cmdvel.angular.z=0.3;
+			 cmdvel.linear.x=0;
+			 cout<<("OBSTACLE AT FRONT\n");
+		}
+		else if
+		{
+			 cmdvel.angular.z=-0.3;
+			 cmdvel.linear.x=0;
+			 cout<<("OBSTACLE AT LEFT\n");
+		}
+	
+	    else
+	    {
+			cmdvel.linear.x=0.6;
+			cmdvel.angular.z=0;
+			cout<<("NO OBSTACLE\n");
+		}
+	 
+	}
+
+}
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "hLaserReader");
-  ros::NodeHandle nodeHandler;
-  ros::Subscriber hokuyoSubscriber = nodeHandler.subscribe("/mybot/laser/scan", 1, laserScanCallback);
-  ros::Publisher pubVelocity = nodeHandler.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+    // Initialising a Node 
+    ros::init(argc, argv, "Laser_readings");
 
-  ros::Rate rate(10);
-  linX = 0.0f;
-  angY = 0.0f;
-  angZ = 0.0f;
-  while (ros::ok()) {
-    geometry_msgs::Twist msg;
-    msg.linear.x = -linX;
-    msg.angular.y = angY;
-    msg.angular.z = angZ;
+    // NodeHandle is the main access point to communications with the ROS system 
+    ros::NodeHandle nh;
 
-    pubVelocity.publish(msg);
-    rate.sleep();
-    ros::spinOnce();
-  }
+    // advertise() function tells ROS to publish on a given topic name 
+    ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
 
-  ros::spin();
+	// The subscribe() tells ROS  to receive messages on a given topic.  
+    ros::Subscriber sub = nh.subscribe("/dd_urdf/laser_scan", 1, callback_laser);
 
-  return 0;
+	// cmdvel parameters
+	 while (ros::ok()) {
+	geometry_msgs::Twist cmdvel;
+  	cmdvel.linear.x=0;
+  	cmdvel.linear.y=0;
+  	cmdvel.linear.z=0;
+  	cmdvel.angular.x=0;
+  	cmdvel.angular.y=0;
+  	cmdvel.angular.z=0; 
+	pub.publish(cmdvel);
+	 }
+    
+
+    // ros::spin() will enter a loop, pumping callbacks 
+    ros::spin();
+    
+return 0;
+
 }
+
